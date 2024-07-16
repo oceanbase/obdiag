d836af6279a664547e9b95ac2d851d2b86d2008f
Teingi <jingshun.tq@antgroup.com>
2024-07-16 16:53:52 +0800

    fix: command auto-completion

--- init_obdiag_cmd.sh
+++ init_obdiag_cmd.sh
@@ -40,7 +40,10 @@ _obdiag_completion() {
                 type_list="list run"
                 COMPREPLY=($(compgen -W "${type_list}" -- "${cur_word}"))
             elif [ "${COMP_WORDS[1]}" = "analyze" ] && [ "${COMP_WORDS[2]}" = "parameter" ]; then
-                type_list="diff non-default"
+                type_list="diff default"
+                COMPREPLY=($(compgen -W "${type_list}" -- "${cur_word}"))
+            elif [ "${COMP_WORDS[1]}" = "analyze" ] && [ "${COMP_WORDS[2]}" = "variable" ]; then
+                type_list="diff"
                 COMPREPLY=($(compgen -W "${type_list}" -- "${cur_word}"))
             fi
             ;;
