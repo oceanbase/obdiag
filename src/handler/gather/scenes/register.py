5d17fdd48e245ed583730164e4534b49fb5bd647
jingshun.tq <35712518+Teingi@users.noreply.github.com>
2025-02-27 14:56:05 +0800

    gather perf add option count & remove zip (#766)
    * Fix the file permission issues under the ~/.obdiag
    
    * gather perf add option count

--- src/handler/gather/scenes/register.py
+++ src/handler/gather/scenes/register.py
@@ -36,7 +36,7 @@ estimated_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 hardcode_scene_list = [
     RegisteredHardCodeScene('observer.perf_sql', f'''obdiag gather scene run --scene=observer.perf_sql --env "{{db_connect='{db_connect}', trace_id='{trace_id}'}}"''', '[SQL performance problem]', '[SQL性能问题]'),
     RegisteredHardCodeScene('observer.sql_err', f'''obdiag gather scene run --scene=observer.sql_err --env "{{db_connect='{db_connect}', trace_id='{trace_id}'}}"''', '[SQL execution error]', '[SQL 执行出错]'),
-    RegisteredHardCodeScene('observer.cpu_high', 'obdiag gather scene run --scene=observer.cpu_high', '[High CPU]', '[CPU高]'),
+    RegisteredHardCodeScene('observer.cpu_high', 'obdiag gather scene run --scene=observer.cpu_high --env "{perf_count=100000000}"', '[High CPU]', '[CPU高]'),
     RegisteredHardCodeScene(
         'observer.px_collect_log', f'''obdiag gather scene run --scene=observer.px_collect_log --env "{{trace_id='{trace_id}', estimated_time='{estimated_time}'}}"''', '[Collect error source node logs for SQL PX]', '[SQL PX 收集报错源节点日志]'
     ),
