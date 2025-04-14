6865499e429921f815071ac3d47dea45fa4ad04f
渠磊 <xuyan.wxy@oceanbase.com>
2025-04-14 21:11:48 +0800

    run ci

--- workflow_data/wait_observer_run.sh
+++ workflow_data/wait_observer_run.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 while true; do
-    ps -aux | grep obd | grep -v grep
+    obclient -h127.0.0.1 -P2881 -uroot -Doceanbase -e "show databases;"
     if [ $? -eq 0 ]; then
-        echo "Process exists, checking again in 5 second..."
-        sleep 5
-    else
         echo "Process not found. Exiting."
         exit 0
+    else
+        echo "Process exists, checking again in 5 second..."
+        sleep 5
     fi
 done
