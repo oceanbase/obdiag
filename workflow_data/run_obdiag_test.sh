39f9a02b68d812f5d4f349dcdb5534bf1a8df20a
渠磊 <xuyan.wxy@oceanbase.com>
2025-04-15 16:41:16 +0800

    fix

--- workflow_data/run_obdiag_test.sh
+++ workflow_data/run_obdiag_test.sh
@@ -7,7 +7,28 @@ function check_error_log {
     exit 1
   fi
 }
+# analyer
+# obdiag analyze log
+check_error_log "obdiag analyze log"
+check_error_log "obdiag analyze log --since 1d"
+check_error_log "obdiag analyze log --scope rootservice"
+check_error_log "obdiag analyze log --grep observer"
+check_error_log "obdiag analyze log --store_dir ./log"
+check_error_log "obdiag analyze log --log_level INFO"
+check_error_log "obdiag analyze log --temp_dir ./log"
 # check
+check_error_log "obdiag check"
 check_error_log "obdiag check list"
 check_error_log "obdiag check run"
+check_error_log "obdiag check run --store_dir ./check"
+check_error_log "obdiag check run --report_type yaml"
+check_error_log "obdiag check run -c ~/config.yml"
+check_error_log "obdiag check run --cases=ad"
+check_error_log "obdiag check run --cases=column_storage_poc"
+check_error_log "obdiag check run --cases=build_before"
+check_error_log "obdiag check run --cases=sysbench_run"
+check_error_log "obdiag check run --cases=sysbench_free"
+check_error_log "obdiag check run --obproxy_cases=proxy"
+# rca
 check_error_log "obdiag rca list"
+
