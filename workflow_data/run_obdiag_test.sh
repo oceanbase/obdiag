9f68678966e953452465d7b985329337dab88b66
渠磊 <xuyan.wxy@oceanbase.com>
2025-04-15 11:27:48 +0800

    run ci

--- workflow_data/run_obdiag_test.sh
+++ workflow_data/run_obdiag_test.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-obdiag check list
-obdiag check run
+set -e
+function check_error_log {
+  output=$($1)
+  if echo "$output" | grep -q "\[ERROR\]"; then
+    echo "Error detected in obdiag output for command: $1. Failing the job."
+    exit 1
+  fi
+}
+# check
+check_error_log "obdiag check list"
+check_error_log "obdiag check run --case=11"
+check_error_log "obdiag rca list"
