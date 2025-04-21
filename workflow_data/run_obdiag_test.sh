98d1d40ef151cff1c8e4998893a69b9269ba9f2f
渠磊 <xuyan.wxy@oceanbase.com>
2025-04-21 16:16:01 +0800

    update ci

--- workflow_data/run_obdiag_test.sh
+++ workflow_data/run_obdiag_test.sh
@@ -107,7 +107,7 @@ check_error_log  "obdiag gather scene run --scene=observer.long_transaction" &
 check_error_log  "obdiag gather scene run --scene=observer.recovery" &
 check_error_log  "obdiag gather scene run --scene=observer.restart" &
 check_error_log  "obdiag gather scene run --scene=observer.rootservice_switch" &
-check_error_log  "obdiag gather scene run --scene=observer.unknown" &
+#check_error_log  "obdiag gather scene run --scene=observer.unknown" &
 check_error_log  "obdiag gather scene run --scene=observer.base" &
 check_error_log  "obdiag gather ash" &
 check_error_log  "obdiag gather ash --report_type TEXT" &
