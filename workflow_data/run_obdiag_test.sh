9211518843375b1a700e77492164c704a1b5868a
渠磊 <xuyan.wxy@oceanbase.com>
2025-04-28 20:36:28 +0800

    ash support html

--- workflow_data/run_obdiag_test.sh
+++ workflow_data/run_obdiag_test.sh
@@ -165,6 +165,7 @@ check_error_log  "obdiag rca list"
 #check_error_log  "obdiag rca run --scene=clog_disk_full"
 #echo "=================obdiag update================="
 #check_error_log  "obdiag update"
+tag=$1
 if [[ "$tag" == "latest" || compare_versions_greater "$tag" "4.2.4.0" ]]; then
     check_error_log  "obdiag gather ash --report_type html"
 wait
