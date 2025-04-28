968b6161b23752aab4e8a141a843d76a6aa98998
渠磊 <xuyan.wxy@oceanbase.com>
2025-04-28 23:31:17 +0800

    ash support html

--- workflow_data/run_obdiag_test.sh
+++ workflow_data/run_obdiag_test.sh
@@ -167,8 +167,14 @@ check_error_log  "obdiag rca list"
 #check_error_log  "obdiag update"
 echo "obdiag gather ash --report_type html"
 echo $tag
-if [[ "$tag" == "latest" || compare_versions_greater "$tag" "4.2.4.0" ]]; then
-    check_error_log  "obdiag gather ash --report_type html"
+# Check if the tag is "latest" or if the version is greater than 4.2.4.0
+is_version_greater=false
+if compare_versions_greater "$tag" "4.2.4.0"; then
+    is_version_greater=true
+fi
+
+if [[ "$tag" == "latest" || "$is_version_greater" == true ]]; then
+    check_error_log "obdiag gather ash --report_type html"
 fi
 wait
 date "+%Y-%m-%d %H:%M:%S"
