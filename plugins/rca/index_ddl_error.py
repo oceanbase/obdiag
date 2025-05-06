dc34e5717eff6312bb0827ec61b60737b5b5d3af
渠磊 <xuyan.wxy@oceanbase.com>
2025-05-06 17:44:55 +0800

    fix rca 913

--- plugins/rca/index_ddl_error.py
+++ plugins/rca/index_ddl_error.py
@@ -61,7 +61,7 @@ class IndexDDLErrorScene(RcaScene):
         self.verbose("tenant_id is {0}".format(self.tenant_id))
         if self.tenant_id is None:
             raise RCAInitException("can not find tenant id by tenant name: {0}. Please check the tenant name.".format(tenant_name))
-        database_id_data = self.ob_connector.execute_sql("select database_id from oceanbase.__all_database where database_name = '{0}';".format(database_name))
+        database_id_data = self.ob_connector.execute_sql("select database_id from oceanbase.__all_virtual_database where database_name = '{0}';".format(database_name))
         if len(database_id_data) == 0:
             raise RCAInitException("can not find database id by database name: {0}. Please check the table name.".format(database_name))
         self.database_id = database_id_data[0][0]
