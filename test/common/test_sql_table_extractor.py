5bec15ba707ae57ab31ba50c85b223358ed97ec3
jingshun.tq <35712518+Teingi@users.noreply.github.com>
2025-06-12 20:22:13 +0800

    Update test_sql_table_extractor.py

--- test/common/test_sql_table_extractor.py
+++ test/common/test_sql_table_extractor.py
@@ -54,6 +54,82 @@ class TestSQLTableExtractor(unittest.TestCase):
         result = self.parser.parse("DROP TABLE IF EXISTS db12.table11;")
         self.assertEqual(result, [])
 
+    def test_subquery(self):
+        sql = """
+        SELECT * FROM db6.main_table 
+        WHERE id IN (SELECT user_id FROM db6.sub_table WHERE status = 'active');
+        """
+        result = self.parser.parse(sql)
+        self.assertEqual(result, [('db6', 'main_table'), ('db6', 'sub_table')])
+
+    def test_multiple_joins(self):
+        sql = """
+        SELECT a.name, b.title, c.content 
+        FROM db7.articles a 
+        JOIN db7.authors b ON a.author_id = b.id 
+        LEFT JOIN db7.comments c ON a.id = c.article_id;
+        """
+        result = self.parser.parse(sql)
+        self.assertEqual(result, [('db7', 'articles'), ('db7', 'authors'), ('db7', 'comments')])
+
+    def test_union_query(self):
+        sql = """
+        SELECT id, name FROM db8.table1 
+        UNION 
+        SELECT id, name FROM db8.table2;
+        """
+        result = self.parser.parse(sql)
+        self.assertEqual(result, [('db8', 'table1'), ('db8', 'table2')])
+
+    def test_mixed_case_sql(self):
+        sql = "SeLeCt * FrOm Db10.TaBlE1 wHeRe Id > 10;"
+        result = self.parser.parse(sql)
+        self.assertEqual(result, [('Db10', 'TaBlE1')])
+
+    def test_multiple_statements(self):
+        sql = """
+        SELECT * FROM db11.table1;
+        INSERT INTO db11.table2 VALUES (1, 'test');
+        UPDATE db11.table3 SET status = 'done';
+        """
+        result = self.parser.parse(sql)
+        self.assertEqual(result, [('db11', 'table1'), ('db11', 'table2'), ('db11', 'table3')])
+
+    def test_complex_nested_subquery(self):
+        sql = """
+        SELECT * FROM db12.outer_table 
+        WHERE id IN (
+            SELECT user_id FROM db12.middle_table 
+            WHERE group_id IN (
+                SELECT id FROM db12.inner_table WHERE status = 'active'
+            )
+        );
+        """
+        result = self.parser.parse(sql)
+        self.assertEqual(result, [('db12', 'outer_table'), ('db12', 'middle_table'), ('db12', 'inner_table')])
+
+    def test_cross_join(self):
+        sql = "SELECT * FROM db13.table1 CROSS JOIN db13.table2;"
+        result = self.parser.parse(sql)
+        self.assertEqual(result, [('db13', 'table1'), ('db13', 'table2')])
+
+    def test_full_outer_join(self):
+        sql = """
+        SELECT * FROM db14.left_table 
+        FULL OUTER JOIN db14.right_table 
+        ON left_table.id = right_table.id;
+        """
+        result = self.parser.parse(sql)
+        self.assertEqual(result, [('db14', 'left_table'), ('db14', 'right_table')])
+
+    def test_empty_query(self):
+        result = self.parser.parse("")
+        self.assertEqual(result, [])
+
+    def test_invalid_sql(self):
+        result = self.parser.parse("SELECT * FROM;")
+        self.assertEqual(result, [])
+
 
 if __name__ == '__main__':
     unittest.main()
