95e9cbd4d0f148d17051519f9529f7deacc67089
Teingi <jingshun.tq@antgroup.com>
2025-06-17 15:28:19 +0800

    Fixed the issue of being unable to create files in the directory passed via the --temp_dir parameter.

--- src/handler/analyzer/analyze_memory.py
+++ src/handler/analyzer/analyze_memory.py
@@ -203,7 +203,11 @@ class AnalyzeMemoryHandler(object):
         to_datetime_timestamp = TimeUtils.timestamp_to_filename_time(TimeUtils.datetime_to_timestamp(self.to_time_str))
         gather_dir_name = "ob_log_{0}_{1}_{2}".format(ssh_client.get_name(), from_datetime_timestamp, to_datetime_timestamp)
         gather_dir_full_path = "{0}/{1}_{2}".format(self.gather_ob_log_temporary_dir, gather_dir_name, str(uuid.uuid4())[:6])
-        mkdir(ssh_client, gather_dir_full_path)
+        mkdir_info = mkdir(ssh_client, gather_dir_full_path)
+        if mkdir_info:
+            resp["skip"] = True
+            resp["error"] = mkdir_info
+            return resp
         log_list, resp = self.__handle_log_list(ssh_client, node, resp)
         result_pack_path = "./{0}".format(os.path.relpath(local_store_dir, self.gather_pack_dir))
         resp["result_pack_path"] = result_pack_path
