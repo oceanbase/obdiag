059eef53f60c87030b09a9a619ac3635f7411a77
jixiaodong <xiaodong.ji@outlook.com>
2025-08-22 07:32:26 +0000

    update report

--- plugins/check/tasks/observer/cluster/data_path_settings.py
+++ plugins/check/tasks/observer/cluster/data_path_settings.py
@@ -38,7 +38,7 @@ class DataPathSettings(TaskBase):
                 self.stdio.verbose(f"Executing on {ip}: {cmd}")
                 data_dir_path = ssher.exec_cmd(cmd).strip()
                 if not data_dir_path:
-                    raise StepResultFailException(f"ip:{ip}, data_dir_path is null. Please check your nodes.data_dir need absolute Path")
+                    self.report.add_warning(f"ip:{ip}, data_dir_path is null. Please check your nodes.data_dir need absolute Path")
 
                 # 获取数据目录所在磁盘
                 # cmd = f"df -h {data_dir_path} | grep '/' | awk '{{print $6}}'"
@@ -52,7 +52,7 @@ class DataPathSettings(TaskBase):
                 self.stdio.verbose(f"Executing on {ip}: {cmd}")
                 log_dir_path = ssher.exec_cmd(cmd).strip()
                 if not log_dir_path:
-                    raise StepResultFailException(f"ip:{ip}, log_dir_path is null. Please check your nodes.redo_dir need absolute Path")
+                    self.report.add_warning(f"ip:{ip}, log_dir_path is null. Please check your nodes.redo_dir need absolute Path")
 
                 # 获取日志目录所在磁盘
                 # cmd = f"df -h {log_dir_path} | grep '/' | awk '{{print $6}}'"
@@ -68,45 +68,45 @@ class DataPathSettings(TaskBase):
 
                 # 检查数据目录和日志目录是否在不同磁盘
                 if data_dir_disk == log_dir_disk:
-                    raise StepResultFailException(f"ip:{ip}, data_dir and log_dir_disk are on the same disk.")
+                    self.report.add_warning(f"ip:{ip}, data_dir and log_dir_disk are on the same disk.")
 
                 # 检查数据目录的文件系统类型 (如果磁盘大小超过16TB，必须是xfs)
                 cmd = f"df -T {data_dir_path} | grep '/' | awk '{{if ($3 > 17179869184 && $2 != \"xfs\") print \"1\"; else print \"0\"}}'"
                 self.stdio.verbose(f"Executing on {ip}: {cmd}")
                 file_system_check = ssher.exec_cmd(cmd).strip()
                 if file_system_check == "1":
-                    raise StepResultFailException(f"ip:{ip}, the data_dir_path of disk size over 16TB, the type must be xfs")
+                    self.report.add_warning(f"ip:{ip}, the data_dir_path of disk size over 16TB, the type must be xfs")
 
                 # 检查日志目录的文件系统类型必须是xfs或ext4
                 cmd = f"df -Th {log_dir_path} | grep '/' | awk '{{print $2}}'"
                 self.stdio.verbose(f"Executing on {ip}: {cmd}")
                 file_system = ssher.exec_cmd(cmd).strip()
                 if file_system not in ["xfs", "ext4"]:
-                    raise StepResultFailException(f"ip:{ip}, log_dir_path: {log_dir_path} file_system is not xfs or ext4.")
+                    self.report.add_warning(f"ip:{ip}, log_dir_path: {log_dir_path} file_system is not xfs or ext4.")
 
                 # 检查日志目录的文件系统类型 (如果磁盘大小超过16TB，必须是xfs)
                 cmd = f"df -T {log_dir_path} | grep '/' | awk '{{if ($3 > 17179869184 && $2 != \"xfs\") print \"1\"; else print \"0\"}}'"
                 self.stdio.verbose(f"Executing on {ip}: {cmd}")
                 file_system_check = ssher.exec_cmd(cmd).strip()
                 if file_system_check == "1":
-                    raise StepResultFailException(f"ip:{ip}, the log_dir_path of disk size over 16TB, the type must be xfs")
+                    self.report.add_warning(f"ip:{ip}, the log_dir_path of disk size over 16TB, the type must be xfs")
 
                 # 检查数据目录的文件系统类型必须是xfs或ext4
                 cmd = f"df -Th {data_dir_path} | grep '/' | awk '{{print $2}}'"
                 self.stdio.verbose(f"Executing on {ip}: {cmd}")
                 file_system = ssher.exec_cmd(cmd).strip()
                 if file_system not in ["xfs", "ext4"]:
-                    raise StepResultFailException(f"ip:{ip}, data_dir_path: {data_dir_path} file_system is not xfs or ext4.")
+                    self.report.add_warning(f"ip:{ip}, data_dir_path: {data_dir_path} file_system is not xfs or ext4.")
 
                 # 所有检查通过
                 self.report.add_success_result(f"ip:{ip}", "data_path_settings", "Check data path settings passed.", {"data_dir": data_dir, "log_dir": redo_dir})
 
             except StepResultFailException as e:
                 self.stdio.warn(str(e))
-                self.report.add_fail(f"ip:{ip}, data_path_settings, {str(e)}")
+                self.report.add_critical(f"ip:{ip}, data_path_settings, {str(e)}")
             except Exception as e:
                 self.stdio.error(f"Check data path settings failed on {ip}: {str(e)}")
-                self.report.add_critical(f"ip:{ip}, data_path_settings, Exception: {str(e)}")
+                self.report.add_fail(f"ip:{ip}, data_path_settings, Exception: {str(e)}")
 
     def get_task_info(self):
         return {
