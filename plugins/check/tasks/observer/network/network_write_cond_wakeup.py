9cab446ec6866e2c73459c7445c7b47da558a77d
suyestyle <35882569+suyestyle@users.noreply.github.com>
2025-08-29 12:14:12 +0800

    Update network_write_cond_wakeup.py
    There is a bug here. 
    
    If 'write cond wakeup' is not found in the log file, 'grep' fails and then 'echo' is triggered, the result will be '0\n0'.
    
    Then, 'easy_slow_count = int(result)' failed

--- plugins/check/tasks/observer/network/network_write_cond_wakeup.py
+++ plugins/check/tasks/observer/network/network_write_cond_wakeup.py
@@ -39,7 +39,7 @@ class NetworkWriteCondWakeupTask(TaskBase):
 
                 # Check observer.log for "write cond wakeup" occurrences
                 log_file_path = "{0}/log/observer.log".format(home_path)
-                check_cmd = "grep -c 'write cond wakeup' {0} 2>/dev/null || echo '0'".format(log_file_path)
+                check_cmd = "grep -c 'write cond wakeup' {0} 2>/dev/null".format(log_file_path)
 
                 result = ssh_client.exec_cmd(check_cmd).strip()
                 self.stdio.verbose("Found {0} 'write cond wakeup' occurrences in {1} on {2}".format(result, log_file_path, ssh_client.get_name()))
